30c901c69cf1e34170f015a47c465e35a4e29840
Thomas Claessens <thomasclaessens1@gmail.com>
2019-04-27 13:30:17 +0200

    Add Language Complete template Improve test

---
+++ src/main/java/be/dominionexperts/cvtool/dto/Language.java
@@ -0,0 +1,50 @@
+package be.dominionexperts.cvtool.dto;
+
+public class Language {
+
+    private String language;
+    private String skillLevel;
+
+    public Language() {
+    }
+
+    private Language(Builder builder) {
+        language = builder.language;
+        skillLevel = builder.skillLevel;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public String getSkillLevel() {
+        return skillLevel;
+    }
+
+    public static Builder newBuilder() {
+        return new Builder();
+    }
+
+
+    public static final class Builder {
+        private String language;
+        private String skillLevel;
+
+        private Builder() {
+        }
+
+        public Builder withLanguage(String language) {
+            this.language = language;
+            return this;
+        }
+
+        public Builder withSkillLevel(String skillLevel) {
+            this.skillLevel = skillLevel;
+            return this;
+        }
+
+        public Language build() {
+            return new Language(this);
+        }
+    }
+}
--- src/test/java/be/dominionexperts/cvtool/templates/TestTemplating.java
+++ src/test/java/be/dominionexperts/cvtool/templates/TestTemplating.java
@@ -1,10 +1,14 @@
 package be.dominionexperts.cvtool.templates;
 
 import be.dominionexperts.cvtool.dto.Basics;
+import be.dominionexperts.cvtool.dto.Education;
+import be.dominionexperts.cvtool.dto.Language;
 import be.dominionexperts.cvtool.dto.Resume;
+import be.dominionexperts.cvtool.dto.Skill;
 import be.dominionexperts.cvtool.util.XDocUtils;
 import fr.opensagres.xdocreport.core.io.IOUtils;
 import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -12,25 +16,24 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class TestTemplating {
 
     @Test
     public void testDocumentGenerationPdf() throws IOException, URISyntaxException {
-        Resume resume = Resume.newBuilder().withBasics(Basics.newBuilder().withName("Thomas").build()).build();
-        URL templateFile = this.getClass().getResource("/Hello.docx");
+        URL templateFile = new ClassPathResource("cv_template.docx").getURL();
         byte[] template = IOUtils.toByteArray(templateFile.openStream());
         String outputPath = Paths.get(templateFile.toURI()).getParent().toString();
-        XDocUtils.generateDocument(template, resume, true).ifPresent(bytes -> save(outputPath, "out.pdf", bytes));
+        XDocUtils.generateDocument(template, getResumeFixture(), true).ifPresent(bytes -> save(outputPath, "out.pdf", bytes));
     }
 
     @Test
     public void testDocumentGenerationDocx() throws IOException, URISyntaxException {
-        Resume resume = Resume.newBuilder().withBasics(Basics.newBuilder().withName("Thomas").build()).build();
-        URL templateFile = this.getClass().getResource("/Hello.docx");
+        URL templateFile = new ClassPathResource("cv_template.docx").getURL();
         byte[] template = IOUtils.toByteArray(templateFile.openStream());
         String outputPath = Paths.get(templateFile.toURI()).getParent().toString();
-        XDocUtils.generateDocument(template, resume, false).ifPresent(bytes -> save(outputPath, "out.docx", bytes));
+        XDocUtils.generateDocument(template, getResumeFixture(), false).ifPresent(bytes -> save(outputPath, "out.docx", bytes));
     }
 
     private static void save(String path, String filename, byte[] bytes){
@@ -45,5 +48,35 @@ public class TestTemplating {
         }catch (Exception ex){
             ex.printStackTrace();
         }
+    }
+
+    private Resume getResumeFixture() {
+        Education education = Education.newBuilder()
+                .withArea("Area")
+                .withStartDate("2010")
+                .withEndDate("2019")
+                .withInstitution("KDG")
+                .withStudyType("Bachelor")
+                .build();
+        Education education2 = Education.newBuilder()
+                .withArea("Area")
+                .withStartDate("2019")
+                .withEndDate("2020")
+                .withInstitution("KUL")
+                .withStudyType("Master")
+                .build();
+
+        Language language = Language.newBuilder().withLanguage("Swahili").withSkillLevel("nihil").build();
+        Language language2 = Language.newBuilder().withLanguage("Nederlands").withSkillLevel("moedertaal").build();
+
+        Skill skill = Skill.newBuilder().withName("Database").withKeywords(List.of("MYSQL", "PostgreSql", "Orcale DB")).build();
+        Skill skill2 = Skill.newBuilder().withName("OS").withKeywords(List.of("Windows 10", "Windows 7", "Windows XP", "Linux")).build();
+
+        return Resume.newBuilder()
+                .withBasics(Basics.newBuilder().withName("Thomas").build())
+                .withEducation(List.of(education,education2))
+                .withLanguages(List.of(language, language2))
+                .withSkills(List.of(skill, skill2))
+                .build();
     }
 }
